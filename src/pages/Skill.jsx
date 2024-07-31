9c35f6ae8eeeea8222bbbe7ec6425a3215d5406d
fuadriyadhi <fuadriyadhi678@gmail.com>
2024-07-31 16:42:47 +0700

    portfolio-progress

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OrbitingCircle from "./../components/ui/CircleSkill";
 
 export default function Skill() {
   return (
@@ -6,11 +7,30 @@ export default function Skill() {
       <div className="about h-[100vh] w-full">
         <div className="flex flex-col justify-center items-center">
           <div data-aos="fade-up" data-aos-duration="1000">
-            <h1 className="font-bold text-[38px] w-[600px] pt-[70px] capitalize text-center">
-              Skill & Experience
-            </h1>
+            <h1 className="font-bold text-[38px] w-[600px] pt-[70px] capitalize text-center">Skill & Experience</h1>
           </div>
-         
+        </div>
+        <div className="container flex justify-items-start ml-[300px] gap-[80px]">
+          <div className="OrbitingCircler">
+            <OrbitingCircle />
+          </div>
+          <div className="experience flex flex-col mt-16">
+            <div className="exp-1">
+              <p className="font-bold">2021</p>
+              <h2>Full Stack Developer</h2>
+              <div className="cpt text-sm w-[400px]">I have a proven track record of delivering high-quality projects and solutions that meet the needs of our clients.</div>
+            </div>
+            <div className="exp-2 mt-8">
+              <p className="font-bold">2021</p>
+              <h2>Full Stack Developer</h2>
+              <div className="cpt text-sm w-[400px]">I have a proven track record of delivering high-quality projects and solutions that meet the needs of our clients.</div>
+            </div>
+            <div className="exp-3 mt-8">
+              <p className="font-bold">2021</p>
+              <h2>Full Stack Developer</h2>
+              <div className="cpt text-sm w-[400px]">I have a proven track record of delivering high-quality projects and solutions that meet the needs of our clients.</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      animation: {
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
+      },
+      keyframes: {
+        orbit: {
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
