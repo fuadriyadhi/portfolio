d9c94d2c46ea3eb75603bcf5d3012579d5042bdf
fuadriyadhi <fuadriyadhi678@gmail.com>
2024-09-29 20:07:33 +0700

    new update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,15 @@ import Card from "./../components/Card";
 
 export default function About() {
   return (
-    <div className="about min-h-screen w-full">
+    <div className="about min-h-screen w-screen">
       <div className="flex flex-col justify-center items-center">
-        <div data-aos="fade-up" data-aos-duration="1000" data-aos-mirror="true" data-aos-offset="200">
-          <h1 className="font-bold text-[2.5rem] pt-[70px] capitalize text-center">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-mirror="true">
+          <h1 className="font-bold text-[2.5rem] pt-[80px] capitalize text-center">
             I Know that <span className="slogan"> high-quality apps </span> <br /> lead to <span className="slogan">successful business</span>
           </h1>
         </div>
         <div className="card">
-          <div className="container mx-auto p-[80px]">
+          <div className="container mx-auto p-[90px]">
             <Card />
           </div>
         </div>
